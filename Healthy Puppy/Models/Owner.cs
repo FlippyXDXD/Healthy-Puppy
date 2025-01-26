4a43850d61f514ae847fa23d8454ab775a4b20d1
JOSfDA <joseflodan@gmail.com>
2025-01-26 11:38:42 -0600

    se edito la parte del owners ya que se estava inserrtando una lista lo que evitava la creacion de  la tabla sql #parche4

--- Healthy Puppy/Models/Owner.cs
+++ Healthy Puppy/Models/Owner.cs
@@ -8,6 +8,5 @@ namespace Healthy_Puppy.Models
         public string Name { get; set; } = string.Empty; // Inicialización para evitar CS8618
         public string Phone { get; set; } = string.Empty; // Inicialización
         public string Email { get; set; } = string.Empty; // Inicialización
-        public List<Dog> Dogs { get; set; } = new(); // Inicialización como lista vacía
     }
 }
