8b2ccb240858cc0fcec61a75552f8c41986ddedc
Flippy <garciajuarezalbertosaid@gmail.com>
2025-01-26 23:42:27 -0600

    Configure algunas utas para facilitar el trabajo

--- Healthy Puppy/MainPage.xaml.cs
+++ Healthy Puppy/MainPage.xaml.cs
@@ -7,33 +7,33 @@ public partial class MainPage : ContentPage
         InitializeComponent();
     }
 
-    // Navegar a la página de registro
-    private async void OnRegisterClicked(object sender, EventArgs e)
-    {
-        await Shell.Current.GoToAsync("//RegistrationPage");
-    }
-
-    // Navegar a la página de lista de dueños
+    // Navegar a la página de información del perro
     private async void OnViewOwnersClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//OwnersListPage");
     }
 
-    // Navegar a la página de control médico (en blanco por ahora)
+    // Navegar a la página de control médico
     private async void OnMedicalControlClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//ControlMedicoPage");
     }
 
-    // Navegar a la página de diagnóstico (en blanco por ahora)
+    // Navegar a la página de diagnóstico
     private async void OnDiagnosisClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("//DiagnosticoPage");
     }
 
-    // Navegar a la página de configuraciones (en blanco por ahora)
+    // Navegar a la página de calendario
+    private async void OnCalendarClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//CalendarioPage");
+    }
+
+    // Navegar a la página de configuraciones
     private async void OnSettingsClicked(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync("//SettingsPage");
+        await Shell.Current.GoToAsync("//ConfiguracionPage");
     }
 }
